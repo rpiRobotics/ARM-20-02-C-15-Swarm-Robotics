f98b75ceeac70ae54e736349305797a6498ffb17
Alex Elias <aelias36@yahoo.com>
2021-06-21 15:26:43 -0400

    Update env_oarbot_silver.sh

--- scripts/env_oarbot_silver.sh
+++ scripts/env_oarbot_silver.sh
@@ -6,9 +6,9 @@ export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.100:11311/
 export ROS_IP=192.168.1.101
-chmod +x /dev/ttyACM0
-chmod +x /dev/ttyACM1
-chmod +x /dev/ttyACM2
-chmod +x /dev/ttyACM3
+# chmod +x /dev/ttyACM0
+# chmod +x /dev/ttyACM1
+# chmod +x /dev/ttyACM2
+# chmod +x /dev/ttyACM3
 
 source $ROS_WS/devel/env.sh
