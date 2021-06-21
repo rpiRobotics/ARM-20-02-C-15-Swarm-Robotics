5a45979b30f1d8cb3aa455e40bdc323667b2a72f
Alex Elias <aelias36@yahoo.com>
2021-06-21 16:24:06 -0400

    add oarbot_blue

---
+++ scripts/env_oarbot_blue.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export ROS_WS=/home/oarbot_blue/catkin_ws
+export ROS_KINETIC=/etc/ros/kinetic
+source $ROS_WS/devel/setup.bash
+source $ROS_KINETIC/setup.bash
+export PATH=$ROS_ROOT/bin:$PATH
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
+export ROS_MASTER_URI=http://192.168.1.100:11311/
+export ROS_IP=192.168.1.102
+chmod +x /dev/ttyACM0
+chmod +x /dev/ttyACM1
+chmod +x /dev/ttyACM2
+chmod +x /dev/ttyACM3
+
+# source $ROS_WS/devel/env.sh
+
+exec "$@"
