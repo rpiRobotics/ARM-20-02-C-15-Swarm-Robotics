daf5d172502e997e13686961f6546e78d698df4c
Alex Elias <aelias36@yahoo.com>
2021-06-21 15:36:07 -0400

    Update env_oarbot_silver.sh

--- scripts/env_oarbot_silver.sh
+++ scripts/env_oarbot_silver.sh
@@ -1,7 +1,8 @@
 export ROS_WS=/home/oarbot_silver/catkin_ws
 export ROS_KINETIC=/etc/ros/kinetic
-source $ROS_WS/devel/setup.sh
-source $ROS_KINETIC/setup.sh
+#source $ROS_WS/devel/setup.bash
+source /home/oarbot_silver/catkin_ws/devel/setup.bash
+source $ROS_KINETIC/setup.bash
 export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.100:11311/
