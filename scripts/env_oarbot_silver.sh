00f68410e8b1128f3fb54cf48b1bd68d3c027d2a
Alex Elias <aelias36@yahoo.com>
2021-06-22 18:54:45 -0400

    source ROS_WS/devel/setup.bash AFTER we source ROS_KINETIC/setup.bash

--- scripts/env_oarbot_silver.sh
+++ scripts/env_oarbot_silver.sh
@@ -2,8 +2,8 @@
 
 export ROS_WS=/home/oarbot_silver/catkin_ws
 export ROS_KINETIC=/opt/ros/kinetic
-source $ROS_WS/devel/setup.bash
 source $ROS_KINETIC/setup.bash
+source $ROS_WS/devel/setup.bash
 export PATH=$ROS_ROOT/bin:$PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
 export ROS_MASTER_URI=http://192.168.1.100:11311/
