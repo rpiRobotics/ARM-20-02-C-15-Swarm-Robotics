5ce1f7d7781635a4a45332ecd4af2ff50e5530e5
Burak Aksoy <burakaksoy20@gmail.com>
2021-06-22 17:40:18 -0400

    update rockie from melodic to kinetic

--- all_devices_catkin_make.bash
+++ all_devices_catkin_make.bash
@@ -2,7 +2,7 @@
 HOSTS=("192.168.1.100" "192.168.1.101" "192.168.1.102" "192.168.1.103")
 USERNAMES=("rockie" "oarbot_silver" "oarbot_blue" "administrator")
 PASSWORDS=("rockie" "1234" "1234" "clearpath")
-SCRIPTS=("source /opt/ros/melodic/setup.bash; cd; cd catkin_ws; catkin_make -DCATKIN_WHITELIST_PACKAGES='swarm_msgs;swarm_gui;swarm_control;swarm_launch'"
+SCRIPTS=("source /opt/ros/kinetic/setup.bash; cd; cd catkin_ws; catkin_make -DCATKIN_WHITELIST_PACKAGES='swarm_msgs;swarm_gui;swarm_control;swarm_launch'"
     "source /opt/ros/kinetic/setup.bash; cd; cd catkin_ws;  catkin_make -DCATKIN_BLACKLIST_PACKAGES='swarm_gui;swarm_control;swarm_launch'"
     "source /opt/ros/kinetic/setup.bash; cd; cd catkin_ws;  catkin_make -DCATKIN_BLACKLIST_PACKAGES='swarm_gui;swarm_control;swarm_launch'"
     "source /opt/ros/kinetic/setup.bash; cd; cd catkin_ws;  catkin_make -DCATKIN_BLACKLIST_PACKAGES='swarm_gui;swarm_control;swarm_launch;oarbot_control'")
