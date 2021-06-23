132e3dfef24e928d018faf767b58647b91a805ea
Alex Elias <aelias36@yahoo.com>
2021-06-23 18:57:48 -0400

    remote launch tablet

---
+++ scripts/env_tablet.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export ROS_WS=/home/tablet/catkin_ws
+export ROS_NOETIC=/opt/ros/noetic
+source $ROS_NOETIC/setup.bash
+source $ROS_WS/devel/setup.bash
+export PATH=$ROS_ROOT/bin:$PATH
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$ROS_WS
+export ROS_MASTER_URI=http://192.168.1.100:11311/
+export ROS_IP=192.168.1.99
+
+exec "$@"
