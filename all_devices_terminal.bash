3ff4072c03d1bd52d0d9da7c99ba110e28de04d2
Burak Aksoy <burakaksoy20@gmail.com>
2021-06-28 13:09:22 -0400

    debug

--- all_devices_terminal.bash
+++ all_devices_terminal.bash
@@ -13,5 +13,6 @@ for i in ${!HOSTS[*]} ; do
     echo ${USERNAMES[i]}
     # echo ${PASSWORDS[i]}
     # echo ${SCRIPTS[i]}
+    ssh-keygen -f "$HOME/.ssh/known_hosts" -R ${HOSTS[i]}
     gnome-terminal --tab --title=${USERNAMES[i]} -e "sshpass -p ${PASSWORDS[i]} ssh -t -o StrictHostKeyChecking=no -o HostKeyAlgorithms='ssh-rsa' -o ConnectTimeout=2 -l ${USERNAMES[i]} ${HOSTS[i]}"
 done
