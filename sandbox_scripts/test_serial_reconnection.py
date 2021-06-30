8da4802b5f28cfba20d6d43420afa1d23a4edafb
Burak Aksoy <burakaksoy20@gmail.com>
2021-06-30 15:17:55 -0400

    test serial reconnection

---
+++ sandbox_scripts/test_serial_reconnection.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import serial
+import time
+
+def serialRead(serialPort):    
+    ser = serial.Serial(serialPort, 115200, timeout=0.04)
+
+    while(True):
+        try:
+            if(ser == None):
+                print("Trying to reconnect")
+                ser = serial.Serial(serialPort, 115200, timeout=0.04)
+                print("Reconnected")
+
+            output = ser.read()
+            print(output)
+
+        except serial.serialutil.SerialException:
+            if(not(ser == None)):
+                ser.close()
+                ser = None
+                print("Disconnecting")
+
+            print("No Connection")
+            time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    serialPort = "/dev/serial/by-path/pci-0000:00:14.0-usb-0:1:1.0"
+    serialRead(serialPort)
+
