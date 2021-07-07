59ec38e0a7ecec3a642d5ed704c6e09d5539540e
Burak Aksoy <burakaksoy20@gmail.com>
2021-07-07 13:49:21 -0400

    debug

--- src/oarbot_control/src/motor_control.py
+++ src/oarbot_control/src/motor_control.py
@@ -37,7 +37,7 @@ class OarbotControl_Motor():
 
         self.velocity_command_sent = True
         self.time_last_motor_cmd = 0.0
-        self.motor_cmd_wait_timeout = 0.3 #seconds
+        self.motor_cmd_wait_timeout = 0.4 #seconds
 
         rospy.Timer(rospy.Duration(0.04), self.motor_feedback)
 
