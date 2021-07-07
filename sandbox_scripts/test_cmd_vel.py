651bf16735f3d9c19a1fca11830369b324db6f50
Burak Aksoy <burakaksoy20@gmail.com>
2021-07-07 11:06:07 -0400

    Add a threaded timer instead of rospy timer

--- sandbox_scripts/test_cmd_vel.py
+++ sandbox_scripts/test_cmd_vel.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+
 import time
+import threading
+class WallTimer():
+    def __init__(self, duration, callback):
+        self.duration = duration
+        self.callback = callback
+
+        self.last_start_time = time.time()
+
+    def run(self):
+        while not rospy.is_shutdown():
+            start_time = time.time()
+            print(start_time - self.last_start_time)
+            self.last_start_time = start_time
+
+            self.callback()
+
+            finish_time = time.time()
+            if (finish_time - start_time) < self.duration:
+                time.sleep(self.duration*1.00 - (finish_time - start_time))
+            else:
+                rospy.logwarn("The loop takes more than defined duration time: " + str(self.duration) +" seconds." )
+        rospy.logwarn("ROS is shutdown" )
+
+    def start(self):
+        threading.Thread(target=self.run).start()
+
 
 class CmdVelTalker():
     def __init__(self):
@@ -10,20 +37,16 @@ class CmdVelTalker():
         self.cmd_vel_pub = rospy.Publisher("/oarbot_silver/e_stop_cmd_vel", Twist, queue_size=1)
         self.direction = 1
 
-        rospy.Timer(rospy.Duration(0.5), self.switch)
+        # rospy.Timer(rospy.Duration(0.5), self.switch)
 
         self.duration = 0.03333 # loop duration
-        print(time.time()) # float in second
-        self.last_start_time = time.time()
+        self.duration2 = 1.0 # switch duration
+
+        self.timer_talk = WallTimer(self.duration,self.talk)
+        self.timer_switch = WallTimer(self.duration2, self.switch)
+
 
     def talk(self):    
-        start_time = time.time()
-        print(start_time - self.last_start_time)
-        self.last_start_time = start_time
-
-        # if (start_time - self.last_start_time) > (self.duration*1.01):
-            # rospy.logwarn("After sleep the loop takes more than defined duration time: " + str(self.duration) +" seconds." )
-
         msg = Twist()
         msg.linear.x = 0.
         msg.linear.y = 0.
@@ -34,19 +57,13 @@ class CmdVelTalker():
 
         self.cmd_vel_pub.publish(msg)
 
-        finish_time = time.time()
-        if (finish_time - start_time) < self.duration:
-            time.sleep(self.duration*1.00 - (finish_time - start_time))
-        else:
-            rospy.logwarn("The loop takes more than defined duration time: " + str(self.duration) +" seconds." )
-
-        
-
-    def switch(self,event):
-        self.direction *= -1 
+    def switch(self):
+        self.direction *= -1
+        rospy.logwarn("Direction changed" )
 
 
 if __name__ == "__main__":
     CmdVelTalker = CmdVelTalker()
-    while not rospy.is_shutdown():
-        CmdVelTalker.talk()
+    CmdVelTalker.timer_talk.start()
+    CmdVelTalker.timer_switch.start()
+    # rospy.spin()
