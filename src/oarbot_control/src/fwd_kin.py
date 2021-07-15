35ebab0cd3ace5851e958ebbbe22e68567957545
Burak Aksoy <burakaksoy20@gmail.com>
2021-07-15 17:53:56 -0400

    add scalings on oarbots

--- src/oarbot_control/src/fwd_kin.py
+++ src/oarbot_control/src/fwd_kin.py
@@ -15,6 +15,9 @@ class OarbotControl_FwdKin():
         self.r          = rospy.get_param("~radius_wheel") # meter
         self.total_gear_ratio  = rospy.get_param("~total_gear_ratio") # rad/s
         self.skid_steer_mode = rospy.get_param("~skid_steer_mode", False) # boolean
+        self.x_vel_scale = rospy.get_param("~x_vel_scaling")
+        self.y_vel_scale = rospy.get_param("~y_vel_scaling")
+        self.th_vel_scale = rospy.get_param("~th_vel_scaling")
 
         self.vel_pub = rospy.Publisher(self.vel_feedback_name, Twist, queue_size=1)
         rospy.Subscriber(self.motor_feedback_topic_name, MotorCmd, self.motor_feedback_callback, queue_size=1)
@@ -39,6 +42,11 @@ class OarbotControl_FwdKin():
         vel_feedback.linear.y = (self.r/4 * (-v_fl + v_fr + v_bl - v_br)) / self.total_gear_ratio
         vel_feedback.angular.z = ((self.r/(4*(self.l_x + self.l_y))) * (-v_fl + v_fr - v_bl + v_br)) / self.total_gear_ratio
         
+        # Scaling
+        vel_feedback.linear.x /= self.x_vel_scale
+        vel_feedback.linear.y /= self.y_vel_scale
+        vel_feedback.angular.z /= self.th_vel_scale
+
         self.vel_pub.publish(vel_feedback)
 
     def forward_kin_skid_steer(self,msg):    
@@ -54,7 +62,12 @@ class OarbotControl_FwdKin():
         vel_feedback.linear.x = (self.r/4 * (v_fl + v_fr + v_bl + v_br)) / self.total_gear_ratio
         vel_feedback.linear.y = 0.0
         vel_feedback.angular.z = (self.r/(4*self.l_y) * (-v_fl + v_fr - v_bl + v_br)) / self.total_gear_ratio
-        
+
+        # Scaling
+        vel_feedback.linear.x /= self.x_vel_scale
+        vel_feedback.linear.y /= self.y_vel_scale
+        vel_feedback.angular.z /= self.th_vel_scale
+
         self.vel_pub.publish(vel_feedback)
 
 if __name__ == "__main__":
--- src/oarbot_control/src/inv_kin.py
+++ src/oarbot_control/src/inv_kin.py
@@ -16,11 +16,18 @@ class OarbotControl_InvKin():
         self.r          = rospy.get_param("~radius_wheel") # meter
         self.total_gear_ratio  = rospy.get_param("~total_gear_ratio") # rad/s
         self.skid_steer_mode = rospy.get_param("~skid_steer_mode", False) # boolean
+        self.x_vel_scale = rospy.get_param("~x_vel_scaling")
+        self.y_vel_scale = rospy.get_param("~y_vel_scaling")
+        self.th_vel_scale = rospy.get_param("~th_vel_scaling")
  
         self.motor_cmd_pub = rospy.Publisher(self.motor_command_name, MotorCmd, queue_size=1)
         rospy.Subscriber(self.teleop_command_name, Twist, self.callback, queue_size=1)
 
     def callback(self, msg):
+        msg.linear.x *= self.x_vel_scale
+        msg.linear.y *= self.y_vel_scale
+        msg.angular.z *= self.th_vel_scale
+
         if self.skid_steer_mode:
             self.inverse_kin_skid_steer(msg)
         else:
